b81f0637d4919cc06bcb58cee5121b28563fff7c
Wisarut Tiyamanee <jtiyamanee@hotmail.com>
2020-06-01 22:50:11 +0700

    finishing sign in handlesubmit method

--- src/App.js
+++ src/App.js
@@ -31,8 +31,7 @@ class App extends React.Component {
               ...snapshot.data()
             }
           });
-          console.log(this.state);
-          
+          // console.log(this.state);
         });
       }
   
